087e46fb2b5105bd80713a914fc35af09b684db4
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-03-19 20:22:55 +0100

    Update main.py

--- PruebasWebScraping/SeleniumPruebas/main.py
+++ PruebasWebScraping/SeleniumPruebas/main.py
@@ -1,34 +1,41 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import sys
+import json
 
-import unittest, time, re
-#https://www.youtube.com/watch?v=st_FvPrir9Q&t=413s
-class Sel(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.compraonline.alcampo.es/categories"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    def test_sel(self):
-        driver = self.driver
-        delay = 3
-        driver.get(self.base_url + "/search?q=stackoverflow&src=typd")
-        driver.find_elements(By.CSS_SELECTOR,"#product-page > div > div > div.Col-sc-1ia5hrt-0.ijpbiH > div > div > div")
-        for i in range(1,100):
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(4)
-        html_source = driver.page_source
-        data = html_source.encode('utf-8')
-        print(data)
+browser = webdriver.Chrome()
+browser.get('https://www.compraonline.alcampo.es/categories')
+
+last_height = browser.execute_script("return document.body.scrollHeight")
 
 
-unittest.main()
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+time.sleep(1)
+
+elements = browser.find_elements(By.CSS_SELECTOR, "#product-page > div > div > div.Col-sc-1ia5hrt-0.ijpbiH > div > div > div")
+
+ids = []
+
+for element in elements:
+
+    id=element.get_attribute("data-visibility-id")
+    if str(id) != "None":
+        ids.append(id)
+
+
+browser.close()
+
+# TODO:     en items tengo todos los ids ahora toca hacer la peticion
+#peticion:
+#www.compraonline.alcampo.es/api/v5/products/decorate?productIds=cddc46c1-7884-4a66-a7f6-ae2533188415,aaa39a2d-7f5b-4627-a92e-6b0950331b72
+print(ids)
+print(str(len(ids)))
+
+
+
+
+
