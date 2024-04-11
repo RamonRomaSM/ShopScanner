5ffd96e4f6ac78e5ad02a9db1c4c8fd9df77b501
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-04-11 12:46:18 +0200

    a

--- Cliente/TrabajoFinalDeGrado/TrabajoFinalDeGrado/obj/Debug/net6.0-windows/funcionalidades/productosFolder/ProductosControl.g.i.cs
+++ Cliente/TrabajoFinalDeGrado/TrabajoFinalDeGrado/obj/Debug/net6.0-windows/funcionalidades/productosFolder/ProductosControl.g.i.cs
@@ -63,14 +63,14 @@ namespace TrabajoFinalDeGrado.funcionalidades.productosFolder {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TrabajoFinalDeGrado;V1.0.0.0;component/funcionalidades/productosfolder/productos" +
-                    "control.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TrabajoFinalDeGrado;component/funcionalidades/productosfolder/productoscontrol.x" +
+                    "aml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\funcionalidades\productosFolder\ProductosControl.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -80,7 +80,7 @@ namespace TrabajoFinalDeGrado.funcionalidades.productosFolder {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
