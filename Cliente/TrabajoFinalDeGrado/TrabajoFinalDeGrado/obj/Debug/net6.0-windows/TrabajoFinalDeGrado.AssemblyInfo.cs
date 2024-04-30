525f5fa52241804aa344fee846c0d635e008ba16
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-04-30 15:00:44 +0200

    a

--- Cliente/TrabajoFinalDeGrado/TrabajoFinalDeGrado/obj/Debug/net6.0-windows/TrabajoFinalDeGrado.AssemblyInfo.cs
+++ Cliente/TrabajoFinalDeGrado/TrabajoFinalDeGrado/obj/Debug/net6.0-windows/TrabajoFinalDeGrado.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabajoFinalDeGrado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fff70d1721a649d35f3ecd39f002fb49e40c68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6217f66bd919e96100662cf1b32d656b6318e8fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabajoFinalDeGrado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabajoFinalDeGrado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
