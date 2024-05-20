a4cfa68971371ccecbfba1680435056495e516ee
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-05-20 22:45:34 +0200

    a

--- WebScraping/PythonScripts/AccesoBdd.py
+++ WebScraping/PythonScripts/AccesoBdd.py
@@ -1,23 +1,34 @@
 import mysql.connector
+import psycopg2
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="nosequeponer"
+POSTGRES_URL=""
+POSTGRES_PRISMA_URL=""
+POSTGRES_URL_NO_SSL=""
+POSTGRES_URL_NON_POOLING=""
+POSTGRES_USER="default"
+POSTGRES_HOST=""
+POSTGRES_PASSWORD=""
+POSTGRES_DATABASE=""
+
+mydb = psycopg2.connect(
+    host=POSTGRES_HOST,
+    dbname=POSTGRES_DATABASE,
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD
 )
 
 def borrarBdd():
 
-    mycursor = mydb.cursor()
+    #mycursor = mydb.cursor()
 
-    mycursor.execute("TRUNCATE TABLE tfg.productos")
+   # mycursor.execute("TRUNCATE TABLE productos")
 
     print("[SISTEMA] Base de datos vaciada")
 
 def guardarProducto(Producto):
     mycursor = mydb.cursor()
 
-    sql = "INSERT INTO tfg.productos (idproductos, nombre, precio, supermercado, oferta, url, imagen) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO productos (idproductos, nombre, precio, supermercado, oferta, url, imagen) VALUES (%s, %s, %s, %s, %s, %s, %s)"
     val = (Producto.id, Producto.nombre, Producto.precio, Producto.supermercado, Producto.oferta, Producto.URL, Producto.imagen)
     mycursor.execute(sql, val)
 
